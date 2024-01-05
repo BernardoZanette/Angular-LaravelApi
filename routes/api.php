ac695f923390723f52033d14fa46ec6456136ad4
BernardoZanette <bernardo7206@gmail.com>
2024-01-05 16:44:42 -0300

    CRUD e suas respectivas Routes usando apiResource()

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/users', [UserController::class, 'index']);
+Route::apiResource('/users', UserController::class);
+// Route::delete('users/{id}', [UserController::class, 'destroy']);
+// Route::patch('users/{id}', [UserController::class, 'update']);
+// Route::get('/users/{id}', [UserController::class, 'show']);
+// Route::get('/users', [UserController::class, 'index']);
+// Route::post('/users', [UserController::class, 'store']);
 
 Route::get('/', function() {
     return response()->json([
